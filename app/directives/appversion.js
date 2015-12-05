d40edbe2993fa1ca64086386af77c886dd35b37a
Damien FREMONT <damien.fremont.job@gmail.com>
2015-12-05 19:56:09 +0100

    added version

--- app/directives/appversion.js
+++ app/directives/appversion.js
@@ -1,14 +1,11 @@
-
 angular.module('app.version', [])
 
 .factory('version', function() {
-    return "0.1.0";
+    return "alpha";
 })
 
 .directive('appVersion', function(version) {
   return {
     template: version
   };
-})
-
-;
+});
