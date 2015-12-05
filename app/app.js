9659ef95f23a17a0374f2ae2d5deb1f0314bfc9c
clement <clement.baudry72@gmail.com>
2015-12-05 15:49:08 +0100

    Ajout feuille de personnage

--- app/app.js
+++ app/app.js
@@ -16,13 +16,14 @@ angular.module('app')
 .config(function(
   $routeProvider,
   $locationProvider) {
-
+  // Define url route to each page
   $routeProvider
 
     .when('/',      {templateUrl: 'app/modules/home/home.html'})
     .when('/about', {templateUrl: 'app/modules/home/about.html'})
     .when('/profile', {templateUrl: 'app/modules/profile/profile.html'})
     .when('/game', {templateUrl: 'app/modules/game/game.html'})
+    .when('/sheet', {templateUrl: 'app/modules/character/sheet.html'})
 
     // Catch all
     .otherwise({redirectTo: '/'});
---
+++ app/modules/character/sheet.js
@@ -0,0 +1,5 @@
+angular.module('app.modules.game', [])
+
+.controller('CharacterSheetCtrl', function($scope) {
+
+});
