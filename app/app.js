eea9acd20fe720490e728d5ab8d5bdf224c49dfe
Damien FREMONT <damien.fremont.job@gmail.com>
2015-12-06 02:23:32 +0100

    updated icon sets

--- app/app.js
+++ app/app.js
@@ -31,7 +31,27 @@ angular.module('app')
 
   // Without server side support html5 must be disabled.
   $locationProvider.html5Mode(false);
-});
+})
+
+.config(['$mdIconProvider', function($mdIconProvider) {
+    $mdIconProvider
+                .iconSet('action', 'img/icons/sets/action-icons.svg', 24)
+                .iconSet('alert', 'img/icons/sets/alert-icons.svg', 24)
+                .iconSet('av', 'img/icons/sets/av-icons.svg', 24)
+                .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24)
+                .iconSet('content', 'img/icons/sets/content-icons.svg', 24)
+                .iconSet('device', 'img/icons/sets/device-icons.svg', 24)
+                .iconSet('editor', 'img/icons/sets/editor-icons.svg', 24)
+                .iconSet('file', 'img/icons/sets/file-icons.svg', 24)
+                .iconSet('hardware', 'img/icons/sets/hardware-icons.svg', 24)
+                .iconSet('icons', 'img/icons/sets/icons-icons.svg', 24)
+                .iconSet('image', 'img/icons/sets/image-icons.svg', 24)
+                .iconSet('maps', 'img/icons/sets/maps-icons.svg', 24)
+                .iconSet('navigation', 'img/icons/sets/navigation-icons.svg', 24)
+                .iconSet('notification', 'img/icons/sets/notification-icons.svg', 24)
+                .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+                .iconSet('toggle', 'img/icons/sets/toggle-icons.svg', 24)
+}]);
 
 angular.module('app')
 .controller('NavCtrl', function($scope, $mdSidenav, $localStorage,
