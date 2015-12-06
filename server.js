685d235bcfc6a46fabbcc0cf190200f04245ec92
Damien FREMONT <damien.fremont.job@gmail.com>
2015-12-06 01:49:21 +0100

    added comments

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-var express = require('express');
-var compression = require('compression');
-var socket = require('./routes/socket.js');
+// Module dependencies.
+var express = require('express'),
+ compression = require('compression'),
+ socket = require('./routes/socket.js');
 
+// Configuration
 var app = express();
+app.use(compression());
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Routes
 app.use('/', express.static('./public'));
-app.use(compression());
 
+// Socket.io Communication
 io.on('connection', socket);
 
+// Start server
 http.listen(5000, function(){
   console.log('listening on *:5000');
 });
